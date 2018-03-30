86e797bd9c375f12b21e720b054956beba3f9136
Danny <liquidcore7@gmail.com>
2018-03-30 23:27:23 +0300

    Initial commit

---
+++ src/main/java/BotInstance.java
@@ -0,0 +1,31 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class BotInstance extends TelegramLongPollingBot {
+    private static final String TOKEN = "546120256:AAFSS0xYUZikndLF9YEuGmXJBO5b9iAza-k";
+    private static final String BOT_USERNAME = "is_down_bot";
+
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return BOT_USERNAME;
+    }
+}
---
+++ src/main/java/MainActivity.java
@@ -0,0 +1,16 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class MainActivity {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi api = new TelegramBotsApi();
+        try {
+            api.registerBot(new BotInstance());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
