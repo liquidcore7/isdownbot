7f7a30c2e88e8648442e5671d18404b503184384
Danny <liquidcore7@gmail.com>
2018-04-01 19:06:31 +0300

    structure changed, availability check impl
    Signed-off-by: Danny <liquidcore7@gmail.com>

--- src/main/java/bot/BotInstance.java
+++ src/main/java/bot/BotInstance.java
@@ -1,11 +1,19 @@
+package bot;
+
+import connectiontest.IsDownChecker;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class BotInstance extends TelegramLongPollingBot {
+    
     private static final String TOKEN = "546120256:AAFSS0xYUZikndLF9YEuGmXJBO5b9iAza-k";
     private static final String BOT_USERNAME = "is_down_bot";
+
+    private void isDownRequestHandler(String url) {
+        IsDownChecker checker = new IsDownChecker(url);
+    }
 
     @Override
     public String getBotToken() {
